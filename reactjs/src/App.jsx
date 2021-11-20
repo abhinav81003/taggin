70d80b729782c720702345650a59d8185afca88e
unknown <d@nielhe.com>
2021-11-20 03:09:18 -0800

    figma to css conversion 1

---
+++ reactjs/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="hashlogo">#</div>
+      <div className="logocontainer">
+        <div className="taggin">taggin'</div>
+        <div className="hash">#</div>
+      </div>
+      <div className="tagline">let your interests find friends</div>
+      <div className="waitlist Rectangle join">join our waitlist</div>
+    </div>
+  );
+}
+
+export default App;
