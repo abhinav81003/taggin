a1a3c31783579fc4423c0cf8ee0b29fffc992973
abhinav81003 <37068167+abhinav81003@users.noreply.github.com>
2021-11-21 15:14:24 -0800

    Add files via upload

---
+++ database.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb+srv://taggin2021:MAmrbwzYYjaKRR26@cluster0.yfj8t.mongodb.net/tags?retryWrites=true&w=majority",{
+    useNewUrlParser: true,
+});
