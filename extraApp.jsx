10f3bafd0bb9c574fc655fcde3a1f71eef712efb
Abhinav Gupta <abhinavgupta2k21@gmail.com>
2021-11-20 16:26:15 -0800

    email box

--- extraApp.jsx
+++ extraApp.jsx
@@ -1,8 +1,6 @@
 import './App.css';
-import Header from './components/header/Header';
 import {useState} from "react";
-import Axios from 'axios';
-
+import Axios from 'axios'
 function App() {
   const [email, setEmail] = useState("");
   const submitEmail = (email) => {
@@ -10,21 +8,13 @@ function App() {
   }
   return (
     <div className="App">
-      <Header/>
-      <div className="logo">
-        <div className="section">
-          <div className="tagginlogo">
-            <div className="taggin">taggin'</div>
-            <div className="logohash">#</div>
-          </div>
-        </div>
-        <div className="section">
-          <div className="tagline">let your interests find friends</div>
-        </div>
-        <div className="section">
-          <div className="waitlist Rectangle join">join our waitlist</div>
-        </div>
+      <div className="hashlogo">#</div>
+      <div className="logocontainer">
+        <div className="taggin">taggin'</div>
+        <div className="hash">#</div>
       </div>
+      <div className="tagline">let your interests find friends</div>
+      <div className="waitlist Rectangle join">join our waitlist</div>
       <div className = "footer">
         <input className="inputEmail" placeholder="Enter Email"
               onChange = {
