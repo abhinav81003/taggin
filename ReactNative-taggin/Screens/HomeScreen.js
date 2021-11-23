0033a9603b21aec1549430cbc04060eeff03104b
Abhinav Gupta <abhinavgupta2k21@gmail.com>
2021-11-22 16:07:40 -0800

    test

--- ReactNative-taggin/Screens/HomeScreen.js
+++ ReactNative-taggin/Screens/HomeScreen.js
@@ -16,7 +16,7 @@ const Home = ({navigation}) => {
     const {width, height} = useDimensions().window;
     const styles = StyleSheet.create({
         map: {width: width,
-        height: height/10,
+        height: height,
         }
     })
     const BottomTabs = createBottomTabNavigator();
