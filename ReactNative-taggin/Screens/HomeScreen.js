e75a71e2c6ac3e5d484a463ebda2ebf8c6ccb707
daniel <d@nielhe.com>
2021-11-22 17:38:00 -0800

    simple purple

--- ReactNative-taggin/Screens/HomeScreen.js
+++ ReactNative-taggin/Screens/HomeScreen.js
@@ -11,6 +11,107 @@ import { useDeviceOrientation, useDimensions } from '@react-native-community/hoo
 import MapView from 'react-native-maps';
 import { Marker } from 'react-native-maps';
 
+
+var mapStyles = [
+    {
+        "featureType": "road",
+        "stylers": [
+            {
+                "hue": "#5e00ff"
+            },
+            {
+                "saturation": -79
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "stylers": [
+            {
+                "saturation": -78
+            },
+            {
+                "hue": "#6600ff"
+            },
+            {
+                "lightness": -47
+            },
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "stylers": [
+            {
+                "lightness": 22
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "stylers": [
+            {
+                "hue": "#eddded"
+            },
+            {
+                "saturation": -11
+            }
+        ]
+    },
+    {},
+    {},
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "saturation": -65
+            },
+            {
+                "hue": "#1900ff"
+            },
+            {
+                "lightness": 8
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "stylers": [
+            {
+                "weight": 1.3
+            },
+            {
+                "lightness": 30
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            },
+            {
+                "hue": "#5e00ff"
+            },
+            {
+                "saturation": -16
+            }
+        ]
+    },
+    {
+        "featureType": "transit.line",
+        "stylers": [
+            {
+                "saturation": -72
+            }
+        ]
+    },
+    {}
+]
+
 const thoughts = [ { title: "Village dining sucks", description: "#usc", latitude: 34.0256, longitude: -118.593 }]
 const Home = ({navigation}) => {
     const {width, height} = useDimensions().window;
@@ -22,16 +123,22 @@ const Home = ({navigation}) => {
     const BottomTabs = createBottomTabNavigator();
     return ( 
         <View>
+            <SafeAreaView>
+                <Text>hi</Text>
+            </SafeAreaView>
              <MapView style={styles.map} region={{longitude: -118.2850,
                         latitude: 34.0256,
                         longitudeDelta: 0.02,
-                        latitudeDelta: 0.02}}>
+                        latitudeDelta: 0.02}} 
+                        provider={MapView.PROVIDER_GOOGLE}
+                        customMapStyle={mapStyles}>
             <MapView.Marker 
                 coordinate={{latitude: 34.0256,
                 longitude: -118.2850,}}
                 title = {"Fuck UCLA"}
                 description ={"#fucla"}
-                image = {style={height: 2,width:2}, source = require('../assets/thought.png')}
+                image = {source = require('../assets/thought.png')}
+                calloutOffset = {{x: 0, y: 30}}
                 />
             </MapView>
         </View>
